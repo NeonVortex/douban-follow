dc1d5cb8aed78276ffb6a346cfbd90c43181e23e
texasbruce <bruce@texasbruce.com>
2018-04-01 22:27:10 -0400

    More functional, more event driven, and use axios for AJAX

--- DoubanFollow.user.js
+++ DoubanFollow.user.js
@@ -6,7 +6,8 @@
 // @author       Mr. Beitang
 // @match        *://www.douban.com/
 // @require      http://code.jquery.com/jquery-1.4.4.min.js
-// @grant        GM_xmlhttpRequest
+// @require      https://unpkg.com/axios/dist/axios.min.js
+// @grant        none
 // @connect      douban.com
 // @license      MIT
 // @run-at       document-end
@@ -15,83 +16,105 @@
 (() => {
     'use strict';
 
-    let link = $("#friend>p").append("<br/><a href='javascript:void(0)'>> 未关注我的人</a><br/>");
+    const link = $("#friend>p").append("<br/><a href='javascript:void(0)'>> 未关注我的人</a><br/>");
+
+    let isLoading = false;
+    let loading = null;
     
     link.click(() => {
-        //20180219 B Change selector for future proof
-        //let numFollowers = $("#friend > p > a[href$='/contacts/rlist']").text().replace(/[^0-9]/g,"");
-        let numFollowers = $("#friend a[href]:contains('被'):contains('人关注')").text().replace(/[^0-9]/g,"");
-        let pagesFollowers = Math.ceil(numFollowers/20);
 
-        //20180219 B Change selector for future proof
-        //let numFollowings = $("#friend .pl > a[href$='/contacts/list']").text().replace(/[^0-9]/g,"");
-        let numFollowings = $("#friend :contains('我的关注') a[href]:contains('成员')").text().replace(/[^0-9]/g,"");
-        let pagesFollowings = Math.ceil(numFollowings/20);
-
-        let followers = [], followings = [];
-
-        let checkNotFollowers = () => {
-            return followings.filter(function(following){return !followers.some(function(follower){return following.id == follower.id;}); });
-        };
-
-        let getLoadingMessageHTML = () => {
-            return "<p><span>正在载入 " +
-                Math.floor(Number(Number(followers.length) + Number(followings.length)) * 100 / Number(Number(numFollowers) + Number(numFollowings))) +
-                "%</span></p>";
-        };
-
-        let loading = $(getLoadingMessageHTML()).insertAfter(link);
-
-        let check = () => {
-            if (followers.length >= numFollowers && followings.length >= numFollowings) {
-                loading.html("");
-                checkNotFollowers().forEach(function(item){
-                    loading.append($("<span>"+item.html.get(0).outerHTML+"</span>"));
-                });
-                loading.append("<br/>");
+        /* const getUserId = callback => {
+            let userId = window.crt_uid;
+            if (typeof userId != 'undefined' && userId) {
+                callback(userId);
             }
             else {
-                loading.html(getLoadingMessageHTML());
+                axios.get('/mine/')
+                    .then(resp => callback(resp.request.responseURL.match(/\/people\/(.+?)\//)[1]))
+                    .catch(resp => showError(resp));
             }
         };
 
-        //20180219 B Compatibility for *Monkey and Chrome Extension
-        let sendRequest = (url, success, error) => {
-            if (typeof GM_xmlhttpRequest != 'undefined' && GM_xmlhttpRequest) {
-                GM_xmlhttpRequest({ method: 'GET', url: url, onload: response => {
-                    success(response.responseText);
-                }, onerror: response => {
-                    error(response.responseText);
-                }});
-            }
-            else {
-                $.ajax({method: 'GET', url: url, success: success, error: error});
-            }
-        };
+        getUserId(userId => console.log(userId));
+        return;
+        */
 
-
-        for (let i = 0; i < pagesFollowers; i++) {
-            //20180219 B setTimeout to avoid Douban's abnormality check
-            setTimeout(() => sendRequest('https://www.douban.com/contacts/rlist?start='+i*20, data => {
-                Array.prototype.push.apply(followers, Array.prototype.map.call($(data).find("ul.user-list>li"), 
-                    item => ({id: $(item).attr("id"), html: $(item).find("a[title]")})));
-                check();
-                }, function(error) {
-                    console.log(error);
-                }), i*500);
+        if (isLoading) {
+            return;
+        }
+        else {
+            isLoading = true;
+            loading && loading.remove();
+            loading = $('<p><span>正在载入</span></p>').insertAfter(link);
         }
 
-        for (let i = 0; i < pagesFollowings; i++) {
-            //20180219 B setTimeout to avoid Douban's abnormality check
-            setTimeout(() => sendRequest('https://www.douban.com/contacts/list?start='+i*20, data => {
-                Array.prototype.push.apply(followings, Array.prototype.map.call($(data).find("ul.user-list>li"), 
-                    item => ({id: $(item).attr("id"), html: $(item).find("a[title]")})));
-                check();
-                }, function(error) {
-                    console.log(error);
-                }), i*500);
-        }
+        const numFollowers = $("#friend a[href]:contains('被'):contains('人关注')").text().replace(/[^0-9]/g,"");
+        const pagesFollowers = Math.ceil(numFollowers/20);
+
+        const numFollowings = $("#friend :contains('我的关注') a[href]:contains('成员')").text().replace(/[^0-9]/g,"");
+        const pagesFollowings = Math.ceil(numFollowings/20);
+
+        const followers = [], followings = [];
+
+ 
+        const getProgressPercentage = () => 
+            Math.floor(Number(Number(followers.length) + Number(followings.length)) * 100 / Number(Number(numFollowers) + Number(numFollowings)));
         
+      
+        const getNoneFollowers = () => 
+            followings.filter(function(following){return !followers.some(function(follower){return following.id == follower.id;}); });
+
+
+        //progress event handler
+        const progressevent = (onprogress, onsuccess) => {
+            if (followers.length >= numFollowers && followings.length >= numFollowings) {
+                onsuccess(getNoneFollowers());
+            }
+            else {
+                onprogress(getProgressPercentage());
+            }
+        };
+
+        const showProgress = progress => {
+            loading.html("<p><span>正在载入 " + progress + '%</span></p>');
+        };
+
+        const showResult = result => {
+            loading.html("");
+            result.forEach(function(item){
+                loading.append($("<span>"+item.html.get(0).outerHTML+"</span>"));
+            });
+            loading.append("<br/>");
+        };
+
+        const showError = error => {
+            console.log(error);
+            $('<p><span>载入失败</span></p>').insertAfter(loading);
+        };
+
+
+        const sendRequest = (url, success, error) => axios.get(url).then(resp=>success(resp.data)).catch(resp=>error(resp));
+
+        const contacts = {followers: {pages: pagesFollowers, pageSize: 20, urlPrefix: 'https://www.douban.com/contacts/rlist?start=', elSel: 'ul.user-list>li'},
+                          followings: {pages: pagesFollowings, pageSize: 20, urlPrefix: 'https://www.douban.com/contacts/list?start=', elSel: 'ul.user-list>li'}};
+
+        for (let key in contacts) {
+            for (let i = 0; i < contacts[key].pages; i++) {
+                setTimeout(() => axios.get(contacts[key].urlPrefix + i * contacts[key].pageSize).then(resp => {
+                    Array.prototype.push.apply(key == 'followers' ? followers : followings, Array.prototype.map.call($(resp.data).find(contacts[key].elSel), 
+                        item => ({id: $(item).attr("id"), html: $(item).find("a[title]")})));
+                    progressevent(progress => {
+                        showProgress(progress);
+                    }, result => {
+                        showResult(result); 
+                        isLoading = false;
+                    });
+                    }).catch(error => {
+                        //TODO clearTimeOut for all jobs when one failed
+                        showError(error);
+                    }), i*500);
+            }
+        }
     });
 
 })();
